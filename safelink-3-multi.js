25ac2383f08e6781f73a28d456ad464b79ab73e4
unduhanel <unduh.anel@gmail.com>
2017-04-17 20:34:59 +0700

    Update safelink-3-multi.js

--- safelink-3-multi.js
+++ safelink-3-multi.js
@@ -19,11 +19,11 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);var x=Math.floor((Math.random()*20)+ 1);var xxx=null;
-if(x=="1"){xxx="/2017/04/akan.html"}
-if(x=="2"){xxx="/2017/04/untuk.html"}
-if(x=="3"){xxx="/2017/04/di.html"}
-if(x=="4"){xxx="/2017/04/ke.html"}
-if(x=="5"){xxx="/2017/04/go.html"}a_to_ve[i].href="http://androidsafelink-ku.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="04/akan.html"}
+if(x=="2"){xxx="04/untuk.html"}
+if(x=="3"){xxx="04/di.html"}
+if(x=="4"){xxx="04/ke.html"}
+if(x=="5"){xxx="04/go.html"}a_to_ve[i].href="http://androidsafelink-ku.blogspot.com/2017/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
